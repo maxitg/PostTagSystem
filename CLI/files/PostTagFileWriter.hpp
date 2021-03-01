5cbf441ef4c0556ad6fe881e35deb19199a5e559
Jesse Friedman <jessef@wolfram.com>
2021-02-28 22:42:56 -0500

    Add (currently unused) methods for reading and writing bit arrays as u64s

--- CLI/files/PostTagFileWriter.hpp
+++ CLI/files/PostTagFileWriter.hpp
@@ -13,10 +13,13 @@ class PostTagFileWriter : public std::ofstream {
   void write_u32(uint32_t n);
   void write_u64(uint64_t n);
 
-  void write_bits(std::vector<bool> bits);
+  void write_bits(const std::vector<bool>& bits);
 
   // bit sequence prefixed by a uint_64 indicating the # of bits
-  void write_prefixed_bits(std::vector<bool> bits);
+  void write_prefixed_bits(const std::vector<bool>& bits);
+
+  // sequence of <= 64 bits written as a 64-bit decimal integer
+  void write_bits_u64(const std::vector<bool>& bits);
 };
 
 #endif  // CLI_FILES_POSTTAGFILEWRITER_HPP_
