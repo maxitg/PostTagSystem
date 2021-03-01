8b5f81b04e04ace20176d7fc1800326422cfe7cf
Jesse Friedman <jessef@wolfram.com>
2021-02-28 22:48:32 -0500

    Revise and document result file format

--- CLI/files/PostTagResultFile.cpp
+++ CLI/files/PostTagResultFile.cpp
@@ -1,8 +1,9 @@
 #include "PostTagResultFile.hpp"
 
-#include <boost/format.hpp>
+#include "TagState.hpp"
+#include "boost/format.hpp"
 
-using PostTagSystem::PostTagHistory;
+using PostTagSystem::PostTagSearcher;
 
 void PostTagResultFileWriter::write_file(const PostTagResultFile& file) {
   // write magic number and version
@@ -29,22 +30,26 @@ void PostTagResultFileWriter::write_file_V1(const PostTagResultFile& file) {
   }
 }
 
-void PostTagResultFileWriter::write_result(const PostTagHistory::EvaluationResult& result, bool write_final_state) {
-  // TODO(jessef): write initial state
+void PostTagResultFileWriter::write_result(const PostTagSearcher::EvaluationResult& result, bool write_final_state) {
+  // write initial state
+  uint8_t initial_state_header = 0;
+  initial_state_header |= (result.initialState.headState & 0b11) << 6;       // bits 0-1: initial head state
+  initial_state_header |= (result.initialState.tape.size() - 1) & 0b111111;  // bits 2-7: initial tape length minus 1
+  write_u8(initial_state_header);
 
-  uint8_t flags = 0;
-  // TODO(jessef): set upper 5 bits of flag to disposition
-  flags |= (write_final_state & 1) << 0;  // last bit
-  write_u8(flags);
+  write_bits(result.initialState.tape);
+
+  uint8_t result_header = 0;
+  result_header |= (static_cast<uint8_t>(result.conclusionReason) & 0b11111) << 3;  // bits 0-4: conclusion reason
+  result_header |= (result.finalState.headState & 0b11) << 1;                       // bits 5-6: final head state
+  result_header |= (write_final_state & 1) << 0;  // bit 7: whether or not final state follows
+  write_u8(result_header);
 
   write_u64(result.eventCount);
   write_u64(result.maxTapeLength);
-
-  write_u64(result.finalState.tape.size());  // TODO(jessef): change to result.finalTapeLength
+  write_u64(result.finalTapeLength);
 
   if (write_final_state) {
-    write_u8(result.finalState.headState);
-
     // no need to write a prefix since we already wrote the final length
     write_bits(result.finalState.tape);
   }
