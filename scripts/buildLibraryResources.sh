c2896f4ecb8231d8109a5e325c1e1e8d8a06f542
Jesse Friedman <jessef@wolfram.com>
2021-03-01 17:25:53 -0500

    Combine Windows CLI build job with main Windows build job

--- scripts/buildLibraryResources.sh
+++ scripts/buildLibraryResources.sh
@@ -39,7 +39,12 @@ echo "libPostTagSystem sources hash: $shortSHA"
 
 mkdir -p build
 cd build
-cmake .. -DPOST_TAG_SYSTEM_ENABLE_ALLWARNINGS=ON -DBUILD_SHARED_LIBS=ON -DCMAKE_BUILD_TYPE=Release
+cmake .. \
+  -DPOST_TAG_SYSTEM_ENABLE_ALLWARNINGS=ON \
+  -DBUILD_SHARED_LIBS=ON \
+  -DCMAKE_BUILD_TYPE=Release \
+  -DPOST_TAG_SYSTEM_BUILD_CLI=${CMAKE_POST_TAG_SYSTEM_BUILD_CLI:-OFF} \
+  -DPOST_TAG_SYSTEM_CLI_STATIC_BUILD=${CMAKE_POST_TAG_SYSTEM_CLI_STATIC_BUILD:-OFF}
 cmake --build . --config Release # Needed for multi-config generators
 cd ..
 
