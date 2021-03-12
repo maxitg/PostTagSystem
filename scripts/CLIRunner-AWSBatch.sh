c1d74c33305c93f1dc44ce042b466497c71d8b6f
Jesse Friedman <jessef@wolfram.com>
2021-03-12 13:23:59 -0500

    Add pounce support to AWS Batch job runner script

--- scripts/CLIRunner-AWSBatch.sh
+++ scripts/CLIRunner-AWSBatch.sh
@@ -12,13 +12,23 @@ outfile_base=${POST_OUTPUT_DIR:-"/tmp/"}
 outfile_path="job/job-$job_id/"
 outfile_name="child-$child_job_index.postresult"
 
+infile_prefix=$POST_INPUT_PREFIX
+
 mkdir -p "$outfile_base$outfile_path"
 
 echo "Arguments: $@"
 echo "Job ID: $job_id"
 echo "Child index: $child_job_index"
 
-wolfram-postproject $@ --initoffset $child_job_index --outfile "$outfile_base""$outfile_path""$outfile_name"
+if [[ "$POST_POUNCE_MODE" == 1 ]]; then
+  wolfram-postproject $@ \
+    --initfile "$infile_prefix""$child_job_index"".postinit" \
+    --outfile "$outfile_base""$outfile_path""$outfile_name"
+else
+  wolfram-postproject $@ \
+    --initoffset $child_job_index \
+    --outfile "$outfile_base""$outfile_path""$outfile_name"
+fi
 
 cli_exit_code=$?
 echo "CLI exit code: $cli_exit_code"
